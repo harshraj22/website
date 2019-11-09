cfe0558d2031a6b7eb97bdb00db1ebef5b062125
Janhavi Dadhania <janhavivdadhania@gmail.com>
2019-11-09 15:24:25 +0530

    Update temp_table.sql

--- temp_table.sql
+++ temp_table.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 09, 2019 at 07:07 AM
+-- Generation Time: Nov 09, 2019 at 03:22 PM
 -- Server version: 5.7.27-0ubuntu0.16.04.1
 -- PHP Version: 7.0.33-0ubuntu0.16.04.7
 
@@ -31,6 +31,17 @@ CREATE TABLE `temp_table` (
   `room` int(11) NOT NULL,
   `branch` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `temp_table`
+--
+
+INSERT INTO `temp_table` (`time`, `room`, `branch`) VALUES
+('8:30 to 9:00', 203, 'Notalloted'),
+('10:00 to 10:30', 109, 'Notalloted'),
+('10:30 to 11:00', 21, 'cse'),
+('11:00 to 12:00', 22, 'Notalloted'),
+('2:30 to 3:30', 212, 'Notalloted');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

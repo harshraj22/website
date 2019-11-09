ccfc00df3bf6ff97836e50612cea53e36591a8f4
Paritosh Gavali <paritosh@.iitdh.ac.in>
2019-11-09 04:29:02 +0000

    added option for adding event' '

---
+++ user/add_event_backend.php
@@ -0,0 +1,69 @@
+<?php
+
+	require_once '../login.php';
+
+	function Error($errno,$errstr){
+		echo "<script type='text/javascript'>alert('ERROR : $errstr');</script>;";
+		die();
+	}
+	set_error_handler("Error");
+
+	session_start();
+	$user = $_SESSION['user'];
+	$logged = $_SESSION['logged'];
+	
+	$title = NULL;
+	$title = $_POST['title'];
+	if($title==NULL){
+		echo "<script type='text/javascript'>alert('You must write title of the event! '); window.location.href = './add_event.php'; </script>";
+	}
+	
+	$date = NULL;
+	$date = $_POST['date'];
+	if($date==NULL){
+		echo "<script type='text/javascript'>alert('Select a date! '); window.location.href = './add_event.php'; </script>";
+	}
+
+	$body = $_POST['body'];
+	$link = $_POST['link'];
+
+	$string = "";
+	$firstcommaerror = 0;
+	if(!empty($_POST['checklist'])){
+		foreach ($_POST['checklist'] as $value) {
+			if(!$firstcommaerror){
+				$string = $value;
+				$firstcommaerror = 1;
+			}
+			else{
+				$string = $string .",".$value;
+			}
+		}
+	}
+
+	if($string==""){
+		echo "<script>alert('Must select atleast one tag!'')</script>";
+	}
+
+	$conn = mysqli_connect($hostname,$username,$password);
+	if(!$conn){
+		echo "error";
+		die();
+		trigger_error("1".mysqli_connect_error());
+	}
+
+	$query = "use ".$database;
+	$result = mysqli_query($conn,$query);
+	if(!$result){
+		trigger_error("2".mysqli_connect_error());
+	}
+
+	$query = "insert into events values ('{$date}','{$title}','{$body}','{$link}','{$string}','{$user}')";
+	$result = mysqli_query($conn,$query);
+	if(!$result){
+		trigger_error("3".mysqli_connect_error());
+	}
+
+	echo "heha";	
+	
+?>

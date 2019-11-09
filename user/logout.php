04ac6f91e41ecd1531cd6d8cb174d2592192aaea
abhiisshheekk <abhiisshheekk@gmail.com>
2019-11-09 19:37:16 +0530

    very special feature

--- user/logout.php
+++ user/logout.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
 session_destroy();
-echo 'You have been logged out. <a href="../auth/auth.php">Go back</a>';
+echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+       <div class="d-flex justify-content-center">
+            <div class="spinner-border text-primary" role="status">
+                <span class="sr-only">Logging out...</span>
+            </div>
+      </div><script>setTimeout(function(){location.href="../auth/auth.php"}, 2000 );</script>';
 ?>

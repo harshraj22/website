455e81e2294bba2597343afc7cb2599c7cb6f81a
Janhavi Dadhania <janhavivdadhania@gmail.com>
2019-11-09 14:53:32 +0530

    Update profile.php

--- user/profile.php
+++ user/profile.php
@@ -53,7 +53,7 @@
     </nav>
      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
 </html>
@@ -108,10 +108,15 @@ _END;
 				    </div>
 					</br>
 					</br>
-					
+
 					<div class="row">
 				    	<a href="logout.php" class="p-3 m-3 btn btn-primary">Logout</a>
 				    </div>
+
+					<div class="row">
+				    	<a href="slot.php" class="p-3 m-3 btn btn-primary">Time Table</a>
+				    </div>
+
 			    </div>
 
 			</body>
